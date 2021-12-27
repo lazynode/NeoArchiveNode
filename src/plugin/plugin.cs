00a2cb64e31a0d83cca4f51389e40c4f436d5f94
Your Name <you@example.com>
2021-12-27 12:01:06 +0000

    dev

---
+++ src/plugin/plugin.cs
@@ -0,0 +1,33 @@
+﻿namespace Neo.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Neo;
+using Neo.IO;
+using Neo.Ledger;
+using Neo.Network.P2P.Payloads;
+using Neo.Persistence;
+
+public class plugin : Plugin, IPersistencePlugin
+{
+    NeoSystem system;
+    IStore store;
+    public override void Dispose()
+    {
+        base.Dispose();
+        system?.EnsureStoped(system.LocalNode);
+        store?.Dispose();
+    }
+    protected override void OnSystemLoaded(NeoSystem ns)
+    {
+        system = ns;
+        store = system.LoadStore(String.Format("Data_Archive_{0}", system.Settings.Network.ToString("X")));
+    }
+    public void OnPersist(NeoSystem ns, Block block, DataCache snapshot, IReadOnlyList<Blockchain.ApplicationExecuted> ae)
+    {
+    }
+    public void OnCommit(NeoSystem system, Block block, DataCache snapshot)
+    {
+    }
+}
