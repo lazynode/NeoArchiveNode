921c9065e36c760e24546faa7e12f343a9a68ab3
Your Name <you@example.com>
2021-12-27 05:29:31 +0000

    dev

--- src/nan/__main__.py
+++ src/nan/__main__.py
@@ -1,5 +1,6 @@
-import code
-import readline
-import rlcompleter
-readline.parse_and_bind("tab: complete")
-code.interact(banner="Welcome to NAN!", local=locals())
+def main():
+    import code
+    import readline
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
+    code.interact(banner="Welcome to NAN!", local=locals())
--- src/nan/example.py
+++ src/nan/example.py
@@ -1,2 +1,10 @@
 def add_one(number):
     return number + 1
+
+
+def main():
+    import code
+    import readline
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
+    code.interact(banner="Welcome to NAN!", local=locals())
