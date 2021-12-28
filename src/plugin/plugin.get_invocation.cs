12adc88f3dc14f6d91c16a1e2262e47564dcfa21
dusmart <dusmart@work.local>
2021-12-28 18:37:44 +0800

    feat(plugin): script eval

---
+++ src/plugin/plugin.get_invocation.cs
@@ -0,0 +1,34 @@
+﻿namespace Neo.Plugins;
+using Neo.VM;
+using Neo.IO.Json;
+using Neo.SmartContract;
+using Neo.Network.P2P.Payloads;
+
+public partial class plugin
+{
+    string get_invocation(string script, string signers)
+    {
+        var signerObjs = JObject.Parse(signers).GetArray().ToArray();
+        Transaction? tx = signerObjs.Length == 0 ? null : new Transaction
+        {
+            Signers = signerObjs.Select(v => new Signer{ Account = UInt160.Parse(v.AsString()) }).ToArray(),
+            Attributes = System.Array.Empty<TransactionAttribute>(),
+            Witnesses = null,
+        };
+        using ApplicationEngine engine = ApplicationEngine.Run(script.HexToBytes(), system.StoreView, container: tx, settings: system.Settings, gas: 20_00000000);
+        JObject json = new();
+        json["script"] = script;
+        json["state"] = engine.State;
+        json["gasconsumed"] = engine.GasConsumed.ToString();
+        try
+        {
+            json["stack"] = new JArray(engine.ResultStack.Select(p => p.ToJson()));
+        }
+        catch (InvalidOperationException)
+        {
+            json["stack"] = "error: invalid operation";
+        }
+        return json.ToString();
+    }
+
+}
---
+++ src/plugin/plugin.get_script.cs
@@ -0,0 +1,12 @@
+﻿namespace Neo.Plugins;
+using Neo.VM;
+using Neo.IO.Json;
+using Neo.SmartContract;
+
+public partial class plugin
+{
+    string get_script(string scripthash, string method, string args)
+    {
+        return UInt160.Parse(scripthash).MakeScript(method, JObject.Parse(args).GetArray().Select(v => ContractParameter.FromJson(v)).ToArray()).ToHexString();
+    }
+}
