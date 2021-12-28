2bba6475d11f29b42b173f07fb4b68321679a0b7
Your Name <you@example.com>
2021-12-28 04:33:35 +0000

    dev

---
+++ build/lib/nan/__entry__.py
@@ -0,0 +1,6 @@
+import sys
+import subprocess
+
+
+def main():
+    subprocess.run([sys.executable, '-m', 'nan'])
---
+++ build/lib/nan/__main__.py
@@ -0,0 +1,11 @@
+import code
+import readline
+import rlcompleter
+from nan.nan import nan
+readline.parse_and_bind("tab: complete")
+code.interact(
+    banner='''welcome to nan''',
+    local={
+        'nan': nan,
+    }
+)
