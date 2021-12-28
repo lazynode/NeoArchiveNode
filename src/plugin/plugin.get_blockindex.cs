236784867cd5b5549cbd8f2ebe829532f606ed0c
Your Name <you@example.com>
2021-12-28 01:42:55 +0000

    dev

---
+++ src/plugin/plugin.get_blockindex.cs
@@ -0,0 +1,10 @@
+﻿namespace Neo.Plugins;
+using Neo.SmartContract.Native;
+
+public partial class plugin
+{
+    string get_blockindex()
+    {
+        return NativeContract.Ledger.CurrentIndex(system!.StoreView).ToString();
+    }
+}
