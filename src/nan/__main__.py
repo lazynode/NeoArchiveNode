d6f9ac7d15226bbe4cdf6f621f5bf371ac24e69c
dusmart <dusmart@work.local>
2021-12-28 19:10:54 +0800

    feat(nan): add history support

--- src/nan/__main__.py
+++ src/nan/__main__.py
@@ -1,9 +1,19 @@
+import os
 import code
 import readline
 import rlcompleter
+import atexit
 from . import nan
 from . import cmd
+historyPath = os.path.expanduser("~/.pyhistory")
+def save_history(historyPath=historyPath):
+    import readline
+    readline.write_history_file(historyPath)
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+atexit.register(save_history)
 readline.parse_and_bind("tab: complete")
+del os, atexit, readline, rlcompleter, save_history, historyPath
 code.interact(
     banner='''welcome to nan''',
     local={
