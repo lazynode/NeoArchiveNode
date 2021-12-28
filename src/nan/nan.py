c9ac8534cd2ce604b4df2380675ae75562e9783a
Your Name <you@example.com>
2021-12-28 02:26:11 +0000

    dev

--- src/nan/nan.py
+++ src/nan/nan.py
@@ -1,10 +1,37 @@
 class Nan:
     def __init__(self) -> None:
-        pass
+        from subprocess import Popen
+        from subprocess import DEVNULL
+        from subprocess import PIPE
+        from os.path import expanduser
+        self.__process = Popen(
+            ['./neo-cli'],
+            cwd=expanduser("~/.nan/neo-cli"),
+            stdin=PIPE,
+            stdout=DEVNULL,
+            stderr=DEVNULL,
+        )
+
+    def __telnet(self, cmd: bytes, *args: bytes) -> bytes:
+        from telnetlib import Telnet
+        with Telnet('localhost', 8517) as tn:
+            tn.write(cmd)
+            tn.write(b'\n')
+            for arg in args:
+                tn.write(arg)
+                tn.write(b'\n')
+            tn.write(b'\n')
+            return tn.read_until(b'\n')[:-1]
 
     @property
-    def exit(self):
+    def exit(self) -> None:
+        self.__process.terminate()
+        self.__process.wait()
         exit()
+
+    @property
+    def blockindex(self) -> int:
+        return int(self.__telnet(b'get_blockindex'))
 
     @property
     def version(self):
