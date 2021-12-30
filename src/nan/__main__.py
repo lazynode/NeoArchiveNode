e41f622dcd8f615b95b55b4fb1b6fe19ad50cf8f
dusmart <dusmart@work.local>
2021-12-30 12:46:05 +0800

    feat(neo-cli): auto downloading dependency file

--- src/nan/__main__.py
+++ src/nan/__main__.py
@@ -1,6 +1,33 @@
 from . import cmd
 from . import nan
 
+
+import os
+if not os.path.exists(os.path.expanduser("~/.nan/neo-cli")):
+    if __import__('platform').machine() != "x86_64":
+        print("Your machine is x86 which is not supported!")
+        exit(1)
+    url = ""
+    if __import__('platform').system() == "Linux":
+        url = "https://github.com/lazynode/NeoArchiveNode/archive/refs/tags/neo-cli-linux-x64-3.1.0.tar.gz"
+        dir_name = "NeoArchiveNode-neo-cli-linux-x64-3.1.0"
+    else:
+        print("Your system is not supported!")
+        exit(1)
+    print("Downloading neo-cli, pelease wait a minute......")
+    import urllib.request
+    import tarfile
+    import shutil
+    file_name, _ = urllib.request.urlretrieve(url)
+    print("Decompressing {}, pelease wait a minute......".format(file_name))
+    with tarfile.open(file_name) as file:
+        file.extractall(file_name+"_extracted")
+    shutil.move(file_name+"_extracted/"+dir_name, os.path.expanduser("~/.nan/neo-cli"))
+    print("neo-cli preparation compelete, enjoy your nan")
+    del urllib.request, tarfile, shutil, url, file_name
+del os
+
+
 __import__('rlcompleter')
 
 try:
@@ -41,6 +68,7 @@ __import__('atexit').register(
         )
     ),
 )
+
 
 __import__('readline').parse_and_bind("tab: complete")
 
