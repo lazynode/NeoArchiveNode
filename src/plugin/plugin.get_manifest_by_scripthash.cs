ed12306d05cb932958e098397328616546e641bb
Your Name <you@example.com>
2021-12-30 04:09:13 +0000

    dev

--- src/plugin/plugin.get_manifest_by_scripthash.cs
+++ src/plugin/plugin.get_manifest_by_scripthash.cs
@@ -3,5 +3,5 @@ using Neo.SmartContract.Native;
 
 public partial class plugin
 {
-    string get_manifest_by_scripthash(string scripthash) => ContractManagement.GetContract(scripthash.UInt160ByScripthash()).Manifest.ToJson().ToString();
+    string get_manifest_by_scripthash(string scripthash) => NativeContract.ContractManagement.GetContract(system!.StoreView, scripthash.UInt160ByScripthash()).Manifest.ToJson().ToString();
 }
